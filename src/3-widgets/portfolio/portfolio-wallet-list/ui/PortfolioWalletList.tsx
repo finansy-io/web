7e0a06837095363d5377e0a6f8bc0bf66c5ceca8
toxa <tonycashbbb@gmail.com>
2025-03-11 17:13:30 +0300

    update props order

--- src/3-widgets/portfolio/portfolio-wallet-list/ui/PortfolioWalletList.tsx
+++ src/3-widgets/portfolio/portfolio-wallet-list/ui/PortfolioWalletList.tsx
@@ -20,6 +20,7 @@ export function PortfolioWalletList() {
 			<TextField value={search} onChange={setSearch} placeholder={APP_TEXT.search} isSearch />
 			<List
 				isLoading={isWalletsLoading}
+				loadingItemWithRightName={false}
 				items={wallets}
 				renderItem={(wallet) => (
 					<Item
@@ -30,7 +31,6 @@ export function PortfolioWalletList() {
 				)}
 				hasNextPage={hasNextWalletsPage}
 				fetchNextPage={fetchNextWalletsPage}
-				loadingItemWithRightName={false}
 			/>
 		</>
 	);
