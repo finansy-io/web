19f20967dfc7b8b281e27c271516b96740528ace
toxa <tonycashbbb@gmail.com>
2025-04-06 16:22:05 +0300

    refactoring UI: update ItemImageWithProgress

--- src/5-shared/ui/item/ui-slots/item-image-with-progress/ui/ItemImageWithProgress.tsx
+++ src/5-shared/ui/item/ui-slots/item-image-with-progress/ui/ItemImageWithProgress.tsx
@@ -1,7 +1,7 @@
 import {type ItemImageWithProgressProps} from '../types/ItemImageWithProgress.types.ts';
 
 export const ItemImageWithProgress = ({image, current, target}: ItemImageWithProgressProps) => {
-	const radius = 73;
+	const radius = 72;
 	const circumference = 2 * Math.PI * radius;
 	const progress = current / target;
 	const strokeDashoffset = circumference * (1 - progress);
@@ -12,14 +12,14 @@ export const ItemImageWithProgress = ({image, current, target}: ItemImageWithPro
 
 			<div className='absolute left-[-13.5px] top-[-13.5px]'>
 				<svg viewBox='0 0 300 300' width='100' height='100'>
-					<circle cx='100' cy='100' r={radius} fill='transparent' stroke='#DFE3E7' strokeWidth='10' />
+					<circle cx='100' cy='100' r={radius} fill='transparent' stroke='#DFE3E7' strokeWidth='9' />
 					<circle
 						cx='100'
 						cy='100'
 						r={radius}
 						fill='transparent'
 						stroke='#805CF5'
-						strokeWidth='10'
+						strokeWidth='9'
 						strokeDasharray={circumference}
 						strokeDashoffset={strokeDashoffset}
 						transform='rotate(-90 100 100)'
