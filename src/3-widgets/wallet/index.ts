645e99bbccea0a3d67534c1e437103d257e93f89
toxa <tonycashbbb@gmail.com>
2025-04-13 13:32:12 +0300

    refactoring UI: update WalletHeader, get rid of WalletDetails

--- src/3-widgets/wallet/index.ts
+++ src/3-widgets/wallet/index.ts
@@ -1,6 +1,5 @@
 export * from '@widgets/wallet/wallets (not using)/ui/Wallets.tsx';
 export * from './wallet-list/ui/WalletList.tsx';
-export * from './wallet-details/ui/WalletDetails.tsx';
 export * from './wallet-header/ui/WalletHeader.tsx';
 export * from './wallet-edit/ui/WalletEdit.tsx';
 export * from './wallet-delete/ui/WalletDelete.tsx';
